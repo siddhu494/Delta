fe08f18cfdcd07608afbcb9f720bb9cbb63cf5cd
siddhu494 <smudigonda@loginsoft.com>
2016-11-08 19:00:49 +0530

    Update Delta1.java

--- DELTA/src/Delta1.java
+++ DELTA/src/Delta1.java
@@ -4,6 +4,7 @@ public class Delta1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is secondary commit");
+		System.out.println("This is secondary commit2");
 	}
 
 }
